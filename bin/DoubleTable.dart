97431b2464a06e94f9b4157b32df637a5ed78a43
flutter-lord <richardolojede2@gmail.com>
2024-09-16 21:19:24 +0100

    commit

--- bin/DoubleTable.dart
+++ bin/DoubleTable.dart
@@ -1,3 +1,13 @@
 void main() {
-  print('Kilogram\t\t\t\t\t\t\t');
+
+  print('Kilograms\t\t\t\t\Pounds\t\t|\t\t\\\tPounds\t\t\t\t\tkilograms');
+
+  for (int i = 1, j = 20; i < 200; j < 516, i += 2, j += 5) {
+
+    double Pounds = 2.2 * i;
+    double kilograms = j / 2.2;
+
+    print('$i\t\t\t\t\t${Pounds.toStringAsFixed(1)}\t\t\|\t\t\t$j\t\t\t\t\t${kilograms.toStringAsFixed(2)}');
+  } 
 }
+
