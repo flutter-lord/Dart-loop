ec00ccaf2232d21df52c19d9fcb56b750580ea32
flutter-lord <richardolojede2@gmail.com>
2024-09-16 14:07:57 +0100

    commit

--- bin/HalfTriangleIndent.dart
+++ bin/HalfTriangleIndent.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 void main() {
 
   for (int line = 1; line <= 6; line++) {
+    
     for (int i = 1; i <= 6 - line; i++) {
       stdout.write('  ');
     }
+
     for (int j = line; j >= 1; j--) {
       stdout.write(' $j');
     }
