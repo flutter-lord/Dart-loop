1bbf9e484f0056ae8cc2a964a445b0800e2c3a4d
flutter-lord <richardolojede2@gmail.com>
2024-09-16 10:20:23 +0100

    commit

--- bin/PositiveNegativeAverage.dart
+++ bin/PositiveNegativeAverage.dart
@@ -14,27 +14,39 @@ void main() {
   int numberOfPositives = 0;
   int numberOfNegatives = 0;
 
+  if (input > 0) {
+      numberOfPositives++;
+  }
+
+  if (input < 0) {
+    numberOfNegatives++; 
+  }
+   
+
   while (input != 0) {
-  stdout.write('Enter an integer, the input ends if it is 0: ');
-  input = int.parse(stdin.readLineSync()!);
+    stdout.write('Enter an integer, the input ends if it is 0: ');
+    input = int.parse(stdin.readLineSync()!);
 
-  sum += input;
+    sum += input;
 
-  if (input > 0) 
-  numberOfPositives += 1;
-  
-  if (input < 0)
-   numberOfNegatives += 1;  
+    if (input > 0) {
+      numberOfPositives ++;
+    }
+
+    if (input < 0) {
+      numberOfNegatives ++; 
+    }
+   
   }
     
   totalNum = numberOfNegatives + numberOfPositives;
   average = sum / totalNum;
 
-   print('The number of positive  is $numberOfPositives');
+   print('The number of positives  is $numberOfPositives');
    print('The number of negatives is $numberOfNegatives');
 
    print('The total is $sum');
 
    print('The average is $average');
     
-   }
+}
