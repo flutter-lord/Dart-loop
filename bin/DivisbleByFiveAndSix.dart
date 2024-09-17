0382b3afddc95b09ad04f3db6bad063659fbb26a
flutter-lord <richardolojede2@gmail.com>
2024-09-17 13:47:31 +0100

    commit

---
+++ bin/DivisbleByFiveAndSix.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+
+  int count = 0;
+
+  for (int number = 100; number <= 1000; number++) {
+    if ((number % 5 == 0 || number % 6 == 0) && !(number % 5 == 0 && number % 6 == 0)) {
+      if (count == 10) {
+        print('');
+        count = 0;
+      }
+      stdout.write('$number ');
+      count++;
+    }
+  }
+
+}
