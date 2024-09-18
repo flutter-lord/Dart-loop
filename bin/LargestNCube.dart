5540b7f3fd3acfb9cc4646a26afd1275c083f777
flutter-lord <richardolojede2@gmail.com>
2024-09-18 12:28:49 +0100

    commit

--- bin/LargestNCube.dart
+++ bin/LargestNCube.dart
@@ -1,14 +1,26 @@
 void main() {
-  int number = 100;
+  int number = 1;
 
-  while ((number * number * number) > 12000) {
-    number --;
-
-    if (number * number * number < 12000) {
-      break;
-    }
+  while ((number * number * number) < 12000) {
+    number++;
   }
+  
+  number--;
+
   print('The largest cube number less than 12000 is $number');
+
+
+
+
+  // while ((number * number * number) > 12000) {
+  //   number --;
+
+  //   if (number * number * number < 12000) {
+  //     break;
+  //   }
+  // }
+  // print('The largest cube number less than 12000 is $number');
+
 }
 
   
