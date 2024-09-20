ccf14bec6c6fa52a48bc16a173b61964e05bc320
flutter-lord <richardolojede2@gmail.com>
2024-09-20 12:58:13 +0100

    commit

--- bin/TrianglePatternD.dart
+++ bin/TrianglePatternD.dart
@@ -3,19 +3,17 @@ import 'dart:io';
 void main() {
 
   for (int line = 1; line <= 6; line++) {
-   for (int j = 5 - (6 - line); j > 0; j++) {
+
+   for (int j = 0; j < line - 1; j++) {
 
      stdout.write(' ');
    }
 
     for (int i = 1; i <= (7 - line); i++) {
 
-     stdout.write('$i ');
+     stdout.write('$i');
 
     }
-
-
-    print('');
-
+    print(''); 
   }
 }
