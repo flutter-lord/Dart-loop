dcf88168a2354e31d65bd298e9db7ce5acc2925c
flutter-lord <richardolojede2@gmail.com>
2024-09-20 21:26:56 +0100

    git commit -m "commit" git push origin main

---
+++ bin/OddnumberSum.dart
@@ -0,0 +1,15 @@
+void main() {
+
+  double sum = 0;
+
+  for (int i = 1, j = (i + 2); i < j; j != 100, i+= 2, j+= 2) {
+    double k = i / j;
+    
+    sum += k;
+
+    
+
+  }
+  print('The sum of the numbers 1/3 + 3/5 +5/7 +.... 97/99  is $sum');
+  
+}
