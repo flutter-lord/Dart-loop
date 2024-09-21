8be5789a9bcb2e1a5ab5d0bbc2e0b492ce9e61ac
flutter-lord <richardolojede2@gmail.com>
2024-09-21 10:22:55 +0100

    commit

--- bin/OddnumberSum.dart
+++ bin/OddnumberSum.dart
@@ -2,15 +2,13 @@ void main() {
 
   double sum = 0;
 
-  for (int i = 1, j = (i + 2); i < j; j != 100, i+= 2, j+= 2) {
+  for (int i = 1, j = 3; j < 100; i += 2, j += 2) {
     
     double k = i / j;
     
     sum += k;
-
-    
-
   }
+
   print('The sum of the numbers 1/3 + 3/5 +5/7 +.... 97/99  is $sum');
   
 }
