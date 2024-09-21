3cce1549cc8e8f5965ccce10e2ba87e819188f0c
flutter-lord <richardolojede2@gmail.com>
2024-09-21 13:12:52 +0100

    commit

---
+++ bin/ValueForE.dart
@@ -0,0 +1,25 @@
+void main () {
+
+  double e = 1;
+   int facorial = 1;
+
+  for (int i = 1; i <= 65; i++) {
+
+    facorial = 1;
+
+    for (int j = i; j >= 1; j--) {
+
+      facorial *= j;
+
+    }
+
+    print("factorial $i: $facorial");
+
+
+    e += (1 / facorial);
+
+  }
+
+  print('Natural Logarithm base is: $e');
+
+}
